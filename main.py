a09a62f01dcc45af759fe23a1dc84aa54d1ae013
Feqan0108 <neceflifeqan183@gmail.com>
2026-07-23 01:33:47 +0400

    Add main.py
    Added function to fetch price data from Twelve Data API and process it into a DataFrame.

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
+import requests
+import pandas as pd
+import ta
+import schedule
+import time
 
+from telegram import Bot
+from config import *
+
+bot = Bot(token=TELEGRAM_TOKEN)
+
+SYMBOL = "XAU/USD"
+INTERVAL = "5min"
+
+last_signal = None
+
+
+def get_price_data():
+
+    url = (
+        f"https://api.twelvedata.com/time_series"
+        f"?symbol={SYMBOL}"
+        f"&interval={INTERVAL}"
+        f"&outputsize=300"
+        f"&apikey={TWELVE_DATA_API_KEY}"
+    )
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    if "values" not in data:
+        print("API Error")
+        return None
+
+    df = pd.DataFrame(data["values"])
+
+    df = df.iloc[::-1]
+
+    df["close"] = df["close"].astype(float)
+    df["high"] = df["high"].astype(float)
+    df["low"] = df["low"].astype(float)
+    df["open"] = df["open"].astype(float)
+
+    return df
